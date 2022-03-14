fbe20377df47b7b12b0b911d5d6d28fb5e015f65
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-14 14:04:57 -0700

    Alphabet with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return(0);
+}
+
