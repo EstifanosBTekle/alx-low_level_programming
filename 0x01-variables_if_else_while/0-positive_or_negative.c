61eacbae7bbc967a7aee838a3833092d4f4231b5
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-14 12:36:46 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,21 +12,21 @@ int main(void)
 int n;
 
 srand(time(0));
-n= rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%i is positive", n);
+printf("%d is positive", n);
 }
 
 else if (n < 0)
 {
-printf("%i is negative", n);
+printf("%d is negative", n);
 }
 
 else
 {
-printf("%i is zero", n);
+printf("%d is zero", n);
 }
 
 return (0);
