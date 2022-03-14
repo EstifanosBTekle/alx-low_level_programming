04d991f97e9f5616e523ad90c38295a4fcd0d12e
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-14 13:40:36 -0700

    Using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+char alphabet;
+
+for (alpabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
