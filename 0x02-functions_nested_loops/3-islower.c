942bc2a7faea77e22f73906dea514c471cb982c6
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-16 06:52:43 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <"main.h"
+
+/**
+ *islower - lower alphabet
+ *
+ * Return: return 1 for lower case
+ */
+
+int _islower(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
