d3b98bad79ef2e7dbf90639516534d18772e2d4b
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-16 07:44:25 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_abs - value absolute
+ *@n: int to manupulate
+ *
+ *Return: return 0
+ */
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else
+{
+return (n);
+}
+
+}
