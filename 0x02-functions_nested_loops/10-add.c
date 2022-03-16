fa87dfe8a0da0053e4168721426de8f7ec187343
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-16 08:47:44 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - adds tow numbers
+ *@a: the first number
+ *@b: the second number
+ *
+ *Return: returns sum
+ */
+
+int add(int a, int b)
+{
+int sum;
+
+sum = a + b;
+
+return (sum);
+}
+
