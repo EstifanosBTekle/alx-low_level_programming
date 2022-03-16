29297253cc601231bbeb00123f60b3da2e221f17
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-16 06:18:31 -0700

    Several times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabet
+ *
+ *Return none
+ */
+void print_alphbet_x10(void)
+{
+int alphabet;
+int i = 0;
+
+while(i<10)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+i++;
+_putchar('\n');
+}
+}
