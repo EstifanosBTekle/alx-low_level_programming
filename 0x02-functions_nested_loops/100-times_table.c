4b38b718f2b8d3df89789c339eb675dcd6ea07d1
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-17 13:03:44 -0700

    First debug

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_times_table - prints time table
+ *@n: parameterr
+ *
+ *Return: returns none
+ */
+
+void print_times_table(int n)
+{
+if (n > 15 && n < 0)
+{
+_putchar('');
+}
+
+else
+{
+	
