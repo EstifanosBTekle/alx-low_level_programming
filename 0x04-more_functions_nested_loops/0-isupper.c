9eb77e42648fa0a3e89c8174be312466db0cd201
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-18 06:03:03 -0700

    Upeercase leters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isupper - Returns 1 for upper case letters
+ *@c: parameter received
+ *
+ *Return: return 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+i
+else
+{
+return (0);
+}
+}
