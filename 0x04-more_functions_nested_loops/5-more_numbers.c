b8635a3efa52a81f99cd28b8370fcbc1d8d7b47c
EstifanosBTekle <estifanosb.tekle@gmail.com>
2022-03-18 07:04:32 -0700

    More numberss

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints more numbers
+ *
+ *Return: return none
+ */
+
+void more_numbers(void)
+{
+int i ;
+
+for (i = 0; i < 10; i++)
+{
+int j;
+
+for (j = 0; j <= 14; j++)
+{
+_putchar (j + '0');
+}
+
+_putchar ('\n');
+}
+
+_putchar ('\n');
+}
